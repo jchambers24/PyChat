142f791ff420cabaa41bbd10ef3b7dae00e84f84
Jack Chambers <jtcroxley@gmail.com>
2019-02-15 12:47:54 -0500

    Added Main

---
+++ SocketServer.py
@@ -0,0 +1,22 @@
+import socket
+
+s = socket.socket()
+host = socket.gethostname()
+port = 12221
+s.bind((host, port))
+
+s.listen(5)
+c = None
+
+while True:
+   if c is None:
+       # Halts
+       print '[Waiting for connection...]'
+       c, addr = s.accept()
+       print 'Got connection from', addr
+   else:
+       # Halts
+       print '[Waiting for response...]'
+       print c.recv(1024)
+       q = raw_input("Enter something to this client: ")
+       c.send(q)
